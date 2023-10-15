4f4a3befdb5c47e47e49e52e2944b8f346985b87
Jianke Yu <yujianke100@gmail.com>
2023-10-15 22:24:08 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         UTS Redirect ACM and IEEE
+// @name         UTS Paper Redirect
 // @namespace    https://github.com/yujianke100/UTS-Paper-Redirect
 // @version      1.0
-// @description  Automatically redirect from ACM and IEEE to UTS Library.
+// @description  Automatically redirect from ACM, IEEE and Springer to UTS Library.
 // @author       Jianke Yu
-// @match        https://dl.acm.org/doi/*
-// @match        https://ieeexplore.ieee.org/document/*
+// @match        https://dl.acm.org/*
+// @match        https://ieeexplore.ieee.org/*
+// @match        https://link.springer.com/*
 // @grant        none
 // @license MIT
 // ==/UserScript==
@@ -16,13 +17,17 @@
     var currentURL = window.location.href;
     var newURL;
 
-    if (currentURL.startsWith("https://dl.acm.org/doi/")) {
+    if (currentURL.startsWith("https://dl.acm.org/")) {
         newURL = currentURL.replace("https://dl.acm.org/", "https://dl-acm-org.ezproxy.lib.uts.edu.au/");
         window.location.href = newURL;
     }
 
-    else if (currentURL.startsWith("https://ieeexplore.ieee.org/document")) {
-        newURL = currentURL.replace("https://ieeexplore.ieee.org/document", "https://ieeexplore-ieee-org.ezproxy.lib.uts.edu.au/document");
+    else if (currentURL.startsWith("https://ieeexplore.ieee.org/")) {
+        newURL = currentURL.replace("https://ieeexplore.ieee.org/", "https://ieeexplore-ieee-org.ezproxy.lib.uts.edu.au/");
+        window.location.href = newURL;
+    }
+    else if (currentURL.startsWith("https://link.springer.com/")) {
+        newURL = currentURL.replace("https://link.springer.com/", "https://link-springer-com.ezproxy.lib.uts.edu.au/");
         window.location.href = newURL;
     }
 })();
