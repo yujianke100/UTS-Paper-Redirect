9f6d8d0a8f08c5b726cfae9c1c26d8b99db39e0f
Jianke Yu <yujianke100@gmail.com>
2024-03-06 19:00:23 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         UTS Library Redirect
-// @namespace    https://github.com/yujianke100/UTS-Library-Redirect
-// @version      1.0
-// @description  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UTS Library.
+// @name         UNSW Library Redirect
+// @namespace    https://github.com/yujianke100/University-Library-Redirect/tree/UNSW
+// @version      1.0.0
+// @description  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UNSW Library.
 // @author       Jianke Yu
 // @match        https://dl.acm.org/*
 // @match        https://ieeexplore.ieee.org/*
@@ -19,17 +19,17 @@
     var newURL;
 
     if (currentURL.startsWith("https://dl.acm.org/")) {
-        newURL = currentURL.replace("https://dl.acm.org/", "https://dl-acm-org.ezproxy.lib.uts.edu.au/");
+        newURL = currentURL.replace("https://dl.acm.org/", "https://dl-acm-org.wwwproxy1.library.unsw.edu.au/");
     }
 
     else if (currentURL.startsWith("https://ieeexplore.ieee.org/")) {
-        newURL = currentURL.replace("https://ieeexplore.ieee.org/", "https://ieeexplore-ieee-org.ezproxy.lib.uts.edu.au/");
+        newURL = currentURL.replace("https://ieeexplore.ieee.org/", "https://ieeexplore-ieee-org.wwwproxy1.library.unsw.edu.au/");
     }
     else if (currentURL.startsWith("https://link.springer.com/")) {
-        newURL = currentURL.replace("https://link.springer.com/", "https://link-springer-com.ezproxy.lib.uts.edu.au/");
+        newURL = currentURL.replace("https://link.springer.com/", "https://link-springer-com.wwwproxy1.library.unsw.edu.au/");
     }
     else if (currentURL.startsWith("https://www.sciencedirect.com/")) {
-        newURL = currentURL.replace("https://www.sciencedirect.com/", "https://www-sciencedirect-com.ezproxy.lib.uts.edu.au/");
+        newURL = currentURL.replace("https://www.sciencedirect.com/", "https://www-sciencedirect-com.wwwproxy1.library.unsw.edu.au/");
     }
     window.location.href = newURL;
 })();
