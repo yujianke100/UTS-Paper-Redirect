0f7bf8043e98afd205df16d9385016f512cff6de
Jianke Yu <yujianke100@gmail.com>
2024-06-05 14:53:27 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UNSW Library Redirect
 // @namespace    https://github.com/yujianke100/University-Library-Redirect/tree/UNSW
-// @version      1.0.1
+// @version      1.1.0
 // @description  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UNSW Library.
 // @author       Jianke Yu
 // @match        https://dl.acm.org/*
@@ -17,19 +17,17 @@
 
     var currentURL = window.location.href;
     var newURL;
+    var proxySuffix = ".wwwproxy1.library.unsw.edu.au";
 
     if (currentURL.startsWith("https://dl.acm.org/")) {
-        newURL = currentURL.replace("https://dl.acm.org/", "https://dl-acm-org.wwwproxy1.library.unsw.edu.au/");
-    }
+            newURL = currentURL.replace("https://dl.acm.org/", "https://dl-acm-org" + proxySuffix + "/") + "?redirected=true";
+        } else if (currentURL.startsWith("https://ieeexplore.ieee.org/")) {
+            newURL = currentURL.replace("https://ieeexplore.ieee.org/", "https://ieeexplore-ieee-org" + proxySuffix + "/") + "?redirected=true";
+        } else if (currentURL.startsWith("https://link.springer.com/")) {
+            newURL = currentURL.replace("https://link.springer.com/", "https://link-springer-com" + proxySuffix + "/") + "?redirected=true";
+        } else if (currentURL.startsWith("https://www.sciencedirect.com/")) {
+            newURL = currentURL.replace("https://www.sciencedirect.com/", "https://www-sciencedirect-com" + proxySuffix + "/") + "?redirected=true";
+        }
 
-    else if (currentURL.startsWith("https://ieeexplore.ieee.org/")) {
-        newURL = currentURL.replace("https://ieeexplore.ieee.org/", "https://ieeexplore-ieee-org.wwwproxy1.library.unsw.edu.au/");
-    }
-    else if (currentURL.startsWith("https://link.springer.com/")) {
-        newURL = currentURL.replace("https://link.springer.com/", "https://link-springer-com.wwwproxy1.library.unsw.edu.au/");
-    }
-    else if (currentURL.startsWith("https://www.sciencedirect.com/")) {
-        newURL = currentURL.replace("https://www.sciencedirect.com/", "https://www-sciencedirect-com.wwwproxy1.library.unsw.edu.au/");
-    }
     window.location.href = newURL;
 })();
