06531434ebcad791d0c9a24356f492e5b1d3d1f7
Jianke Yu <yujianke100@gmail.com>
2024-06-05 15:07:47 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // ==UserScript==
 // @name         UNSW Library Redirect
+// @name:en      UNSW Library Redirect
+// @name:zh-CN   重定向至UNSW图书馆
 // @namespace    https://github.com/yujianke100/University-Library-Redirect/tree/UNSW
-// @version      1.1.0
+// @version      1.1.1
+// @description  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UNSW Library.
 // @description:en  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UNSW Library.
 // @description:zh  自动将ACM、IEEE、Springer和ScienceDirect网站重定向至UNSW图书馆。
 // @author       Jianke Yu
@@ -11,6 +14,8 @@
 // @match        https://www.sciencedirect.com/*
 // @grant        none
 // @license MIT
+// @downloadURL https://update.greasyfork.org/scripts/489131/UNSW%20Library%20Redirect.user.js
+// @updateURL https://update.greasyfork.org/scripts/489131/UNSW%20Library%20Redirect.meta.js
 // ==/UserScript==
 
 (function() {
