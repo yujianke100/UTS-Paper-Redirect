c6e33b001ed784308f3cf564c41d976d2b86498f
Jianke Yu <yujianke100@gmail.com>
2024-06-16 15:09:29 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
 // ==UserScript==
 // @name         UNSW Library Redirect
-// @name:en      UNSW Library Redirect
-// @name:zh-CN   重定向至UNSW图书馆
 // @namespace    https://github.com/yujianke100/University-Library-Redirect/tree/UNSW
-// @version      1.1.3
-// @description  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UNSW Library.
-// @description:en  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UNSW Library.
-// @description:zh-CN  自动将ACM、IEEE、Springer和ScienceDirect网站重定向至UNSW图书馆。
-// @author       Jianke Yu
-// @match        https://dl.acm.org/*
-// @match        https://ieeexplore.ieee.org/*
-// @match        https://link.springer.com/*
-// @match        https://www.sciencedirect.com/*
-// @grant        none
-// @license MIT
-// ==/UserScript==
- 
-// ==UserScript==
-// @name         UNSW Library Redirect
-// @name:en      UNSW Library Redirect
-// @name:zh-CN   重定向至UNSW图书馆
-// @namespace    https://github.com/yujianke100/University-Library-Redirect/tree/UNSW
-// @version      1.1.2
+// @version      1.1.4
 // @description  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UNSW Library.
 // @description:en  Automatically redirect from ACM, IEEE, Springer and ScienceDirect to UNSW Library.
 // @description:zh-CN  自动将ACM、IEEE、Springer和ScienceDirect网站重定向至UNSW图书馆。
@@ -45,7 +25,7 @@
 
     // Function to check if the URL has already been redirected
     function isRedirected(url) {
-        return url.includes(proxySuffix) || url.includes("redirected=true");
+        return url.includes(proxySuffix) || url.includes("redirected=true") || url.includes("%3Fredirected%3Dtrue") || url.includes("arnumber=");
     }
 
     if (!isRedirected(currentURL)) {
